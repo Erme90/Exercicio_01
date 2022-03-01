2f8666698645a7fe7f25b552cd35c976f250b67b
Emerson Freire <emersonnascimento.freire@gmail.com>
2022-03-01 08:56:22 -0300

    Adicionado opção de continuar jogando

--- Ex_01.py
+++ Ex_01.py
@@ -5,24 +5,14 @@ from time import sleep
 print('Vamos jogar JO KEN PÔ!')
 print('Iniciando o jogo!')
 #sleep(2)
+
 opcoes = ('PEDRA', 'PAPEL', 'TESOURA')
-randomizador = randint(0, 2)
-computador = (opcoes[randomizador])
+while True:
+    randomizador = randint(0, 2)
+    computador = (opcoes[randomizador])
+    print('PEDRA, PAPEL OU TESOURA?')
+    jogador = input('Resposta: ').strip().upper()
 
-# Bloco do jogadores
-print('PEDRA, PAPEL OU TESOURA? ')
-jogador = input('> ').strip().upper()
-resp = ('')
-while resp != ('NÃO', 'N'):
-    while jogador not in ('pedra', 'PEDRA', 'Pedra',
-                          'papel', 'PAPEL', 'Papel'
-                        'tesoura', 'TESOURA', 'Tesoura'):
-        print('Opção inválida!')
-        jogador = input('PEDRA, PAPEL OU TESOURA? ').strip().upper()
-
-        # Bloco das condicionais (Jogador joga pedra)
-
-    # Bloco "PEDRA"
     if jogador == ('PEDRA') and computador == ('PEDRA'):
         print(f'Computador escolheu {computador}.')
         print('EMPATE!!!')
@@ -32,6 +22,8 @@ while resp != ('NÃO', 'N'):
     elif jogador == ('PEDRA') and computador == ('TESOURA'):
         print(f'Computador escolheu {computador}.')
         print('Você venceu!')
+
+
 
     #Bloco "PAPEL"
     elif jogador == ('PAPEL') and computador == ('PAPEL'):
@@ -44,6 +36,7 @@ while resp != ('NÃO', 'N'):
         print(f'Computador escolheu {computador}.')
         print('Você venceu!')
 
+
     #Bloco "TESOURA"
     elif jogador == ('TESOURA') and computador == ('TESOURA'):
         print(f'Computador escolheu {computador}.')
@@ -54,6 +47,9 @@ while resp != ('NÃO', 'N'):
     elif jogador == ('TESOURA') and computador == ('PAPEL'):
         print(f'Computador escolheu {computador}.')
         print('Você venceu!')
-    resp = input('Jogar novamente? ').upper()
 
-#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
+    repete = input('Deseja continuar? ').strip().upper()
+    if repete == 'N':
+        break
+
+print('Jogo finalizado')
